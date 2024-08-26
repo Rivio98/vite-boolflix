f7afc1e229ad69c1aac527e61ad2bca53cd5a4b7
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-26 17:13:31 +0200

    [UPD] Add functions for Api call series and movies

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
   components: {
     AppHeader,
     AppMain,
+    SearchBar
   },
   created() {
   },
--- src/store.js
+++ src/store.js
@@ -1,6 +1,36 @@
 import { reactive } from 'vue'
+import axios from 'axios';
 
 export const store = reactive({
     apiUrlmovie: "https://api.themoviedb.org/3/discover/movie",
-    apiUrlseries: "https://api.themoviedb.org/3/discover/tv"
+    apiUrlseries: "https://api.themoviedb.org/3/discover/tv",
+    apiKey: '6a6afbe4a45c3a0d643a9ae389aba2d0',
+
+    async searchMovie(movieName) {
+        try {
+            const response = await axios.get(`${this.apiUrlmovie}?api_key=${this.apiKey}&query
+            = ${movieName}`);
+            return response.data.results
+        }
+
+        catch (error) {
+            console.error("Errore nella ricerca del film:", error);
+            return null;
+        }
+
+    },
+    async searchSeries(seriesName) {
+        try {
+            const response = await axios.get(`${this.apiUrlseries}?api_key=${this.apiKey}&query
+         = ${seriesName}`);
+            return response.data.results
+        }
+
+        catch (error) {
+            console.error("Errore nella ricerca della serie:", error);
+            return null;
+        }
+
+    }
+
 });
