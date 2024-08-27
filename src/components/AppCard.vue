2ec7ca4d44d17b8a9c164c3d2e6e32175e9bfaea
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-27 15:14:49 +0200

    [UPD] Added methods for flag-icons co corrispond film original language to flag and finished Milestone 2

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,25 +1,51 @@
 <script>
 import { store } from '../store.js';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+const languageToCountryMap = {
+    en: 'us', // English -> United States
+    it: 'it', // Italian -> Italy
+    fr: 'fr', // French -> France
+    es: 'es', // Spanish -> Spain
+    de: 'de', // German -> Germany
+    ja: 'jp', // Japanese -> Japan
+    zh: 'cn', // Chinese -> China
+    ko: 'kr', // Korean -> South Korea
+    ru: 'ru', // Russian -> Russia
+    pt: 'pt', // Portuguese -> Portugal
+    ar: 'sa', // Arabic -> Saudi Arabia
+};
+
 export default {
     data() {
         return {
             store,
         };
     },
-}
+    methods: {
+        getCountryName(countryCode) {
+            const country = this.store.countries.find(country => country.alpha2Code === countryCode);
+            return country ? country.name : '';
+        },
+        getFlagClass(languageCode) {
+            const countryCode = languageToCountryMap[languageCode] || languageCode;
+            return `fi fi-${countryCode}`;
+        },
+    },
+};
 </script>
+
 <template>
     <div v-if="store.results.length > 0">
         <h2>Risultati della ricerca:</h2>
         <ul>
             <li v-for="item in store.results" :key="item.id">
                 {{ item.title || item.name }} ({{ item.vote_average }})
-                ({{ item.original_title }}) ({{ item.original_language }})
+                ({{ item.original_title }}) <span :class="getFlagClass(item.original_language)"></span>
             </li>
         </ul>
     </div>
 </template>
 
 <style lang="">
-
 </style>
