a015f40b3e592468ad65dd7bbc5b26d399b79e85
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-27 16:48:26 +0200

    [UPD] Add methods for add stars from average vote

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -15,11 +15,22 @@ const languageToCountryMap = {
     pt: 'pt', // Portuguese -> Portugal
     ar: 'sa', // Arabic -> Saudi Arabia
 };
+const getStarClass = (voteAverage, starIndex) => {
+    const ratingOutOfFive = (voteAverage / 2);
+    const fullStars = Math.floor(ratingOutOfFive);
+
+    if (starIndex <= fullStars) {
+        return 'bi bi-star-fill text-warning';
+    } else {
+        return 'bi bi-star';
+    }
+};
 
 export default {
     data() {
         return {
             store,
+            getStarClass
         };
     },
     methods: {
@@ -38,21 +49,34 @@ export default {
 <template>
     <div v-if="store.resultsMovies.length > 0">
         <h2>Risultati Film:</h2>
-        <ul>
+        <ul class="list-unstyled">
             <li v-for="item in store.resultsMovies" :key="item.id">
-                <img :src="`https://image.tmdb.org/t/p/w500/${item.poster_path}`" alt="">
-                {{ item.title || item.name }} ({{ item.vote_average }})
+                <img
+                    :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.title}`">
+
+                <h1>{{ item.title }}</h1>
                 ({{ item.original_title }}) <span :class="getFlagClass(item.original_language)"></span>
+                <div class="rating">
+                    <span v-for="n in 5" :key="n">
+                        <i :class="getStarClass(item.vote_average, n)"></i>
+                    </span>
+                </div>
             </li>
         </ul>
     </div>
     <div v-if="store.resultsSeries.length > 0">
         <h2>Risultati delle Serie TV:</h2>
-        <ul>
+        <ul class="list-unstyled">
             <li v-for="item in store.resultsSeries" :key="item.id">
-                <img :src="`https://image.tmdb.org/t/p/w500/${item.poster_path}`" alt="">
-                {{ item.title || item.name }} ({{ item.vote_average }})
+                <img
+                    :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.name}`">
+                <h1>{{ item.name }}</h1>
                 ({{ item.original_title }}) <span :class="getFlagClass(item.original_language)"></span>
+                <div class="rating">
+                    <span v-for="n in 5" :key="n">
+                        <i :class="getStarClass(item.vote_average, n)"></i>
+                    </span>
+                </div>
             </li>
         </ul>
     </div>
