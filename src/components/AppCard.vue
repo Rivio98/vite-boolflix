eab2c0719b967aee9323af7b1a70ce4bb2d8a2d9
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-28 10:49:36 +0200

    [UPD] mileston 4 done

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -47,47 +47,24 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="contain">
         <div class="row">
             <div class="col-12">
                 <div v-if="store.resultsMovies.length > 0">
-                    <h2>Risultati Film:</h2>
-                    <div class="row row-cols-3">
-                        <div v-for="item in store.resultsMovies" :key="item.id" class="card" style="max-width: 540px;">
-                            <div class="row g-0">
-                                <div class="col-md-4">
-                                    <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.title}`"
-                                        class="img-fluid rounded-start">
+                    <h2 class="text-light">Risultati Film:</h2>
+                    <div class="row row-cols-3 justify-content-center">
+                        <div v-for="item in store.resultsMovies" :key="item.id" class="card p-0 m-3 movie-card">
+                            <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.title}`"
+                                class="img-fluid movie-poster">
+                            <div class="card-body movie-info">
+                                <div class="d-flex">
+                                    <h5 class="card-title pe-2">{{ item.title }}</h5>
+                                    <span :class="getFlagClass(item.original_language)"></span>
                                 </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{{ item.title }}</h5> ({{ item.original_title }}) <span
-                                            :class="getFlagClass(item.original_language)"></span>
-                                        <p class="card-text">{{ item.overview }}</p>
-                                        <span v-for="n in 5" :key="n">
-                                            <i :class="getStarClass(item.vote_average, n)"></i>
-                                        </span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div v-if="store.resultsSeries.length > 0">
-                <h2>Risultati Serie TV:</h2>
-                <div class="row row-cols-3">
-                    <div v-for="item in store.resultsSeries" :key="item.id" class="card" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.name}`"
-                                    class="img-fluid rounded-start">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ item.name }}</h5> ({{ item.original_title }}) <span
-                                        :class="getFlagClass(item.original_language)"></span>
-                                    <p class="card-text">{{ item.overview }}</p>
+                                <p v-if="item.original_title !== item.title" class="original-title">
+                                    ({{ item.original_title }})</p>
+                                <p class="card-text">{{ item.overview }}</p>
+                                <div class="rating">
                                     <span v-for="n in 5" :key="n">
                                         <i :class="getStarClass(item.vote_average, n)"></i>
                                     </span>
@@ -97,8 +74,119 @@ export default {
                     </div>
                 </div>
             </div>
+            <div v-if="store.resultsSeries.length > 0">
+                <h2 class="text-light">Risultati Serie TV:</h2>
+                <div class="row row-cols-3 justify-content-center">
+                    <div v-for="item in store.resultsSeries" :key="item.id" class="card p-0 m-3 movie-card">
+                        <img :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : `https://placehold.co/342x513?text=${item.name}`"
+                            class="img-fluid movie-poster">
+                        <div class="card-body movie-info">
+                            <div class="d-flex">
+                                <h5 class="card-title pe-2">{{ item.name }}</h5>
+                                <span :class="getFlagClass(item.original_language)"></span>
+                            </div>
+                            <p v-if="item.original_name !== item.name" class="original-title">
+                                ({{ item.original_name }})
+                            </p>
+                            <p class="card-text">{{ item.overview }}</p>
+                            <div class="rating">
+                                <span v-for="n in 5" :key="n">
+                                    <i :class="getStarClass(item.vote_average, n)"></i>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
-<style lang="">
+<style scoped>
+.contain {
+    margin: 0 auto;
+    background-color: #333;
+    padding: 20px;
+}
+
+.movie-card {
+    width: 200px;
+    height: 300px;
+    position: relative;
+    overflow: hidden;
+    border-radius: 10px;
+    transition: transform 0.3s ease-in-out;
+    background-color: #222;
+}
+
+.movie-card:hover {
+    transform: scale(1.05);
+}
+
+.movie-poster {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 10px;
+}
+
+.movie-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.8);
+    color: white;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    overflow-y: auto;
+}
+
+.movie-card:hover .movie-info {
+    opacity: 1;
+}
+
+.card-title,
+.original-title {
+    margin-bottom: 5px;
+    font-size: 16px;
+    text-align: center;
+}
+
+.card-text {
+    font-size: 14px;
+    margin-bottom: 10px;
+    text-align: center;
+    max-height: 100px;
+    overflow-y: auto;
+    padding-right: 10px;
+}
+
+.card-text::-webkit-scrollbar {
+    width: 8px;
+}
+
+.card-text::-webkit-scrollbar-track {
+    background: #333;
+    border-radius: 10px;
+}
+
+.card-text::-webkit-scrollbar-thumb {
+    background: #666;
+    border-radius: 10px;
+}
+
+.card-text::-webkit-scrollbar-thumb:hover {
+    background: #888;
+}
+
+.card-text {
+    scrollbar-width: thin;
+    scrollbar-color: #666 #333;
+}
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -29,11 +29,67 @@ export default {
 };
 </script>
 
-<template>
-    <div>
-        <input v-model="searchQuery" placeholder="Cerca un film o una serie..." />
-        <button @click="searchContent">Cerca</button>
+<template lang="">
+    <div class="search-container justify-content-between">
+        <div class="brand-name">
+            <h1 class="title">BOOLFLIX</h1>
+        </div>
+        <div class="search-wrapper">
+            <input v-model="searchQuery" placeholder="Cerca un film o una serie..." />
+            <button @click="searchContent">Cerca</button>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.search-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    background-color: #111010;
+    padding: 20px;
+}
+
+.search-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+}
+
+.search-container input {
+    padding: 10px;
+    border: 2px solid #444;
+    border-radius: 5px;
+    background-color: #222;
+    color: #fff;
+    width: 250px;
+}
+
+.search-container button {
+    padding: 10px 15px;
+    border: none;
+    border-radius: 5px;
+    background-color: #ff5a5f;
+    /* Button color */
+    color: white;
+    font-size: 16px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+}
+
+.search-container button:hover {
+    background-color: #e03e3e;
+}
+
+.brand-name {
+    margin-left: 20px;
+    font-size: 24px;
+    font-weight: bold;
+    color: #fff;
+}
+
+.title {
+    color: red;
+}
+</style>
